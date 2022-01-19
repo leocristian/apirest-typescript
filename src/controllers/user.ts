b9a68b6c088a3657cce7dc1b34c5cae69ebbd69c
leocristian <leonardosclopes@gmail.com>
2022-01-19 09:59:26 -0300

    add password encryption

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,24 +1,46 @@
 import { Request, Response } from "express"
 import { ObjectId } from "mongodb";
 import User from "../schemas/User";
+import bcrypt from "bcrypt"
+
 
 class UserController {
+
     async login(req: Request, res: Response) {
         console.log("entrou");
         
-        const user = await User.find({ name: req.body.name})
+        const user = await User.findOne({ name: req.body.name})
         console.log(user)
-        if(!user) {
+
+        const comparison = await bcrypt.compareSync(req.body.password, user.password)
+
+        if(!comparison) {
             return res.status(404).send("User not found!")
         } else {
-
             return res.status(200).send("User found!")
         }
     }
     async signup(req: Request, res: Response) {
 
-        const result = await User.create(req.body)
+        const passwordReq = req.body.password
+
+        const saltRouds = 10
+        const generatedSalt = await bcrypt.genSaltSync(saltRouds)
         
+        const hash = bcrypt.hashSync(passwordReq, generatedSalt)
+
+        const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        }
+        
+        if(await User.findOne({ email: newUser.email })) {
+            return res.status(409).send("User already exists")
+        }
+
+        const result = await User.create(newUser)
+
         if(!result) {
             return res.status(500).send("Error on signup")
         } else {
